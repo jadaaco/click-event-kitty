5859ee2342b817391cdfb7197c68326b13f60c16
Jada Condrey <140356974+jadaaco@users.noreply.github.com>
2023-09-04 20:19:37 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function(){
+    button.innerText = "It's working.";
+    cat.classList.add("show")
+});
